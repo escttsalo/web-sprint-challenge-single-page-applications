e36dbae977d6d0104fd76131bfdbd07c86feb0dc
escttsalo <escttsalo@gmail.com>
2021-05-21 15:41:37 -0600

    start app make header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
+import { Route, Link } from 'react-router-dom'
 
 const App = () => {
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <div className='container'>
+      <nav>
+        <h1 className='header'>Lambda Eats</h1>
+        <div className='nav-links'>
+          <a href="#">Home</a>
+          {/* <Link to='/' id='order-pizza'>Home</Link> */}
+          <a href="#">Help</a>
+        </div>
+      </nav>
+
+      
+    </div>
   );
 };
 export default App;
